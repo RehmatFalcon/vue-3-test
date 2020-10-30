aa458ac3b09ead82fd6473f9177d3d637faf27e5
Kushal Niroula <niroulaKushal31@gmail.com>
2020-10-30 21:44:38 +0545

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <input type="text" v-model="name" />
+    <button @click.prevent="Reset">Reset</button>
+    <payment></payment>
+  </div>
+</template>
+
+<script>
+import Payment from "./Payment";
+
+const initial = {
+  name: "Name",
+};
+export default {
+  components: {
+    payment: Payment,
+  },
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return initial;
+  },
+  methods: {
+    Reset() {
+      this.$data = { ...this.$data, ...initial };
+      alert(this.$data.name);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Payment.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>Payment</h3>
+  Paid: <input type="number" v-model="paymentData.paid" /> <br />
+  Discount:
+  <input type="number" v-model="paymentData.discount" />
+  <br />
+  Total:
+  <input type="number" disabled v-model="totalPaid" />
+</template>
+<script lang="ts">
+import { reactive, computed } from "vue";
+export default {
+  setup() {
+    const paymentData = reactive({
+      paid: 0,
+      discount: 0,
+    });
+    const totalPaid = computed(
+      () =>
+        parseFloat(paymentData.paid || 0) +
+        parseFloat(paymentData.discount || 0)
+    );
+    return {
+      paymentData,
+      totalPaid,
+    };
+  },
+};
+</script>
